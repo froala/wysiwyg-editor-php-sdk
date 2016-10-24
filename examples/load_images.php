c0d789bee2f49213c9cb7a182ef742823ec32b6b
Stefan Neculai <stefan.neculai@gmail.com>
2016-10-24 15:26:03 +0300

    Remove examples tracked files.

--- examples/load_images.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/froala/wysiwyg-editor-php-sdk/lib/froala_editor.php';
-
-$response = FroalaEditor_Image::getList('/examples/uploads/');
-
-echo stripslashes(json_encode($response));
-?>
